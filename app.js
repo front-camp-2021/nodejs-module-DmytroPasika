d6b7e64f5c3e57c94161a06b1fe629318ea4eb3f
Dmytro Pasika <dmytro.pasika@gmail.com>
2021-11-23 22:31:45 +0200

    2 task complete

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
-const http = require("http");
-
+const express = require('express');
+const fs = require('fs');
+const app = express();
 const port = 5000;
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  if (url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    res.write('<h1>Greeting from server!</h1>');
-    res.end();
+const randomText = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (var i = 0; i <= charactersLength; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
+  return result.split(/\d+/).join(' ');
+}
 
-  if (url === '/data') {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    res.write('<p>Welcome to data path</p>');
-    res.end();
-  }
-});
-
-server.listen(port, () => {
-  console.log(`Server is listening on port - ${port}`)
+app.get("/", (req, res) => {
+  res.send('<h1>Greeting from server!</h1>');
 })
+
+app.get("/data", (req, res) => {
+  res.send('<p>Welcome to data path</p>');
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening at port - ${port}`)
+})
+
+fs.writeFile("./text/file.txt", '', (err) => {
+  if (err) throw err;
+  for (let i = 1; i <= 10; i++) {
+    fs.appendFileSync("./text/file.txt", `${i === 1 ? i : '\n' + i}`, (err) => {
+      if (err) throw err;
+    })
+  }
+})
+
+fs.writeFile("./text/random_text.txt", randomText(), (err) => {
+  if (err) throw err;
+
+  fs.readFile("./text/random_text.txt", "utf-8", (err, data) => {
+    if (err) throw err;
+
+    console.log('random text --> ', data)
+
+    fs.unlink("./text/random_text.txt", (err) => {
+      if (err) console.log(err);
+    });
+  })
+})
+
+
+
+
+
+
+
